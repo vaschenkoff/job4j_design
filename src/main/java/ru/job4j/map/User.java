700c72c0f3403973da5ac2396751943213eef173
vaschenkoff <vaschenkoff@mail.ru>
2024-02-21 16:12:05 +0300

    4. Без переопределения equals и hashCode [#1005 #482519]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,9 +1,44 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
     private int children;
     private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "name='" + name + '\''
+                + ", children=" + children
+                + ", birthday=" + birthday
+                + '}';
+    }
+
+    public static void main(String[] args) {
+        Calendar birthday = Calendar.getInstance();
+        User user1 = new User("Ivan", 15, birthday);
+        User user2 = new User("Ivan", 15, birthday);
+        Map<User, Object> map = new HashMap<>(16);
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        System.out.println(map);
+        int hashcode1 = user1.hashCode();
+        int hash1 = hashcode1 ^ (hashcode1 >>> 16);
+        int bucket1 = hash1 & 15;
+        System.out.println("user1: hashcode = " + hashcode1 + " hash = " + hash1 + " bucket = " + bucket1);
+        int hashcode2 = user2.hashCode();
+        int hash2 = hashcode2 ^ (hashcode2 >>> 16);
+        int bucket2 = hash2 & 15;
+        System.out.println("user2: hashcode = " + hashcode2 + " hash = " + hash2 + " bucket = " + bucket2);
+    }
 }
