48831936474381bc09acc2f6b296375cdbdcb000
vaschenkoff <vaschenkoff@mail.ru>
2024-02-21 16:26:05 +0300

    7. Переопределить и equals и hashCode [#1002]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -25,13 +25,17 @@ public class User {
                 + '}';
     }
 
-   /* @Override
+   @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         User user = (User) o;
         return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
-    } */
+    }
 
     @Override
     public int hashCode() {
