ac64b1f79436eb248aff436977997ccb96cc2d9b
vaschenkoff <vaschenkoff@mail.ru>
2024-02-22 11:51:49 +0300

    3. Создать модель User [#999 #482517]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -10,11 +10,11 @@ public class User {
     private int children;
     private Calendar birthday;
 
-    public User(String name, int children, Calendar birthday) {
+    /*public User(String name, int children, Calendar birthday) {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
-    }
+    } */
 
     @Override
     public String toString() {
